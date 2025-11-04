24e5cf42565110a7a0c2d63554734751477f8094
ACER <ACER@DESKTOP-AO608KQ>
2025-11-04 23:09:35 +0330

    Add fluentvalidator

---
+++ FlightBooking.Application/Validators/SearchRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FlightBookingApplication.DTOs.Search;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingApplication.Validators
+{
+    public class SearchRequestValidator : AbstractValidator<SearchRequestDto>
+    {
+        public SearchRequestValidator() 
+        {
+            RuleFor(x => x.Origin).NotEmpty().WithMessage("مبدا الزامی است.");
+            RuleFor(x => x.Destination).NotEmpty().WithMessage("مقصد الزامی است.")
+                .NotEqual(x => x.Origin).WithMessage("مبدا و مقصد نباید یکسان باشند.");
+
+
+        }
+    }
+}
